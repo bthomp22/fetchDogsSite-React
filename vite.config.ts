69b2c136e582e4b72ebe1507b5283cd0b6fc3f36
bthomp22 <brentthompson2012@gmail.com>
2025-02-11 22:05:03 -0500

    Update vite.config.ts
    update for new build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/fetchDogsSite-React/",
   plugins: [
     react(),
     tailwindcss(),
 
   ],
-})
+  
+});
